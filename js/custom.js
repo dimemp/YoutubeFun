b93ec63ab7e53766b0a4e63e17aeb0cf2edb7720
Dimitris Dimos <dimitrisdimosiaris@gmail.com>
2016-08-08 22:26:47 +0300

    First commit

---
+++ js/custom.js
@@ -0,0 +1,48 @@
+var channelName = 'CrazyTsach';
+
+$(document).ready(function(){
+
+    $.get (
+        "https://www.googleapis.com/youtube/v3/channels", {
+            part: 'contentDetails',
+            forUsername: channelName,
+            key: 'AIzaSyCqHtArKSUrBpUnbdVMMKQYU5bsJVeptdc'
+        },
+
+        function(data){
+            $.each(data.items, function(i, item){
+                console.log(item);
+                var pid = item.contentDetails.relatedPlaylists.uploads;
+                getVideos(pid);
+            })
+        }
+    );
+
+    function getVideos(pid) {
+
+        $.get (
+        "https://www.googleapis.com/youtube/v3/playlistItems", {
+            part: 'snippet',
+            maxresults: 10,
+            playlistId: pid,
+            key: 'AIzaSyCqHtArKSUrBpUnbdVMMKQYU5bsJVeptdc'
+        },
+
+        function(data){
+            var output;
+            $.each(data.items, function(i, item){
+                console.log(item);
+                var videoTitle = item.snippet.title;
+                var videoId = item.snippet.resourceId.videoId
+                var output = '<li><iframe src="//www.youtube.com/embed/'+videoId+'"></iframe></li>';
+
+                //Append to results playlist
+                $('#results').append(output);
+            })
+        }
+    );
+
+    }
+});
+
+

17aeae83f1e6dbab96fef8efb1bc5463db2c3e4b
Dimitris Dimos <dimitrisdimosiaris@gmail.com>
2016-08-16 17:19:53 +0300

    Final fix

--- YoutubeFun/settings.py
+++ YoutubeFun/settings.py
@@ -106,5 +106,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/static'),
+    os.path.join(BASE_DIR, 'static'),
 )

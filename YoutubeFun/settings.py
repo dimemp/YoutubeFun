437aaf018feaf969d2ba1ebf8fd7ea803a9fe459
Dimitris Dimos <dimitrisdimosiaris@gmail.com>
2016-08-23 01:27:06 +0300

    Serve staticfiles for heroku

--- YoutubeFun/settings.py
+++ YoutubeFun/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xb103+!$k5uhqga^9$^g=t^bw-3zo-6j4c+cf8x8mflhq-*qm2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -108,3 +108,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'Youtube_app/static'),
 )
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'

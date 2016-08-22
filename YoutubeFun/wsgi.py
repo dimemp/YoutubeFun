e3752075f25751b963a7438a62b20f647d3af9e4
Dimitris Dimos <dimitrisdimosiaris@gmail.com>
2016-08-23 01:34:13 +0300

    Try

--- YoutubeFun/wsgi.py
+++ YoutubeFun/wsgi.py
@@ -15,8 +15,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "YoutubeFun.settings")
 
 application = get_wsgi_application()
 
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)

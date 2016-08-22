b2f1c73631300f941cc97fa09f59017cd17c15ad
Dimitris Dimos <dimitrisdimosiaris@gmail.com>
2016-08-23 01:03:37 +0300

    Add all video channels

--- Youtube_app/static/js/landing.js
+++ Youtube_app/static/js/landing.js
@@ -1,15 +1,15 @@
-// Hides all video descriptions
-$('.video-content').hide();
+/* Hides all video descriptions
+$( document ).ready(function() {
+	$('.video-content').hide();
 
-$('.video-clip').on('mouseenter',function(event) {
+	$('.video-clip').on('mouseenter',function(event) {
 
-    $(this).find('.play-overlay').fadeIn('slow');
-    $(this).find('.video-content').slideToggle('slow');
+	    $(this).find('.video-content').slideToggle('slow');
 
-}).on('mouseleave', function(event) {
+	}).on('mouseleave', function(event) {
 
-    $(this).find('.play-overlay').fadeOut('slow');
-    $(this).find('.video-content').slideToggle('slow');
-    
-});
+	    $(this).find('.video-content').slideToggle('slow');
+	    
+	});
+});	
 
--- Youtube_app/static/js/youtube_api.js
+++ Youtube_app/static/js/youtube_api.js
@@ -1,49 +1,97 @@
-var channelName = 'CrazyTsach';
-var videoResults = 10;
+var channelPosition = {
+    'CrazyTsach': '#Crazy-Tsach-results',
+    'VibratorProductions': '#Vibrator-Productions-results',
+    'AstathiosPranksMain': '#Astathios-Pranks-results',
+    'BooyahchannelGR': '#Booyah-Tv-results'
+};
+
+var videoResults = 4;
+var latestVideo = '';
+var latestVideoDescription ='';
+var latestVideoId = '';            
 
 $(document).ready(function(){
 
+    $.each(channelPosition, function( channel, id_position ) {
+        
     $.get (
         "https://www.googleapis.com/youtube/v3/channels", {
             part: 'contentDetails',
-            forUsername: channelName,
+            forUsername: channel,
             key: 'AIzaSyCqHtArKSUrBpUnbdVMMKQYU5bsJVeptdc'
         },
 
         function(data){
             $.each(data.items, function(i, item){
-                console.log(item);
                 var pid = item.contentDetails.relatedPlaylists.uploads;
                 getVideos(pid);
             })
         }
     );
 
+    //Get videos playlist latest items
     function getVideos(pid) {
-
+        
         $.get (
         "https://www.googleapis.com/youtube/v3/playlistItems", {
             part: 'snippet',
-            maxresults: videoResults,
+            maxResults: videoResults,
             playlistId: pid,
             key: 'AIzaSyCqHtArKSUrBpUnbdVMMKQYU5bsJVeptdc'
         },
 
         function(data){
-            var output;
-            $.each(data.items, function(i, item){
-                console.log(item);
-                var videoTitle = item.snippet.title;
-                var videoId = item.snippet.resourceId.videoId
-                var output = '<li><iframe src="//www.youtube.com/embed/'+videoId+'"></iframe></li>';
 
-                //Append to results playlist
-                $('#results').append(output);
+            var output;
+            var latestVideoOutput;
+
+            $.each(data.items, function(i, item){
+                console.log(item)
+                var videodescription = item.snippet.description;
+                var videodate = item.snippet.publishedAt;
+                var videoId = item.snippet.resourceId.videoId;
+                
+                //Create the HMTL for video rows
+                var output = '<div class="col-md-3 video-clip"><div class="video"><iframe src="//www.youtube.com/embed/'+videoId+'"></iframe></div><div class="video-content"><div class="time"><span class="post-date"><i class="fa fa-clock-o time-icon" aria-hidden="true"></i>'+videodate+'</span></div><div class="description">'+videodescription+'</div></div></div>';
+
+                $(id_position).append(output);
+
+                //Hide video description and date
+                $('.video-content').hide();
+
+                $('.video-clip').on('mouseenter',function(event) {
+
+                    $(this).find('.video-content').slideToggle('slow');
+
+                }).on('mouseleave', function(event) {
+
+                    $(this).find('.video-content').slideToggle('slow');
+                    
+                });
+
+                //Find the latest video
+                if (videodate > latestVideo) {
+
+                    latestVideo = videodate;
+                    latestVideoDescription = videodescription;
+                    latestVideoId = videoId;
+                    
+                };
+
+
             })
         }
-    );
+    );}
 
-    }
+    });
+
+    //Create the HMTL for latest video row
+    var latestVideoOutput = '<div id="latest-top-video"><iframe src="//www.youtube.com/embed/'+latestVideoId+'"></iframe></div>';
+                    
+    //Append to results playlist
+    $('#latest-video-iframe').append(latestVideoOutput);
+    $('#latest-video-description').append(latestVideoDescription);
+
 });
 
 
